65ece2068b10d4764545d7aeca642cc744609cf8
PoQuatre <leflem.mateo2002@gmail.com>
2022-03-07 12:18:13 +0100

    Add basic styling

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
+import "@fontsource/oswald";
+import "@fontsource/poppins";
+import "./index.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
